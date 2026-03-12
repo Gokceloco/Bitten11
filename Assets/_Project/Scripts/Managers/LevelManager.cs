d9f18e5f5fe4a4462a36390eed79820db595b707
gokcegun <gokce@locomotive.games>
2026-03-12 19:29:35 +0300

    LevelManager Added

---
+++ Assets/_Project/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public GameDirector gameDirector;
+
+
+    public void RestartLevelManager()
+    {
+        DeleteOldLevel();
+        CreateNewLevel();
+    }
+
+    private void DeleteOldLevel()
+    {
+
+    }
+    private void CreateNewLevel()
+    {
+
+    }    
+}
